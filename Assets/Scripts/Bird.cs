a2bedd0acccd3ade359381507207a5fa44e0085c
Marcelo Chaves <marcelochaves17@gmail.com>
2019-03-22 20:13:09 -0300

    Start project

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float mass = 5;
+    public float maxVelocity;
+
+    public Vector3 velocity = Vector3.zero;
+    public Vector3 force;
+    private Vector3 position;
+    private Vector3 gravity = new Vector3(0, -9.81f);
+
+    private void Start()
+    {
+        position = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        UpdateVelocity();
+        transform.position = position;
+    }
+
+    private void UpdateVelocity()
+    {
+        Vector3 acc = force / mass + gravity;
+        velocity = velocity + acc * Time.deltaTime;
+        position = position + velocity * Time.deltaTime;
+        force = Vector3.zero;
+    }
+
+    private void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            if (velocity.y < 3.0f)
+                force = new Vector3(0, 3000, 0);
+        
+        this.Death();
+    }
+
+    public void Death()
+    {
+        if (this.transform.position.y < -3)
+        {
+            print("Morreu!");
+            Time.timeScale = 0;
+        }
+    }
+}
