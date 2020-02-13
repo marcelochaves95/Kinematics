cb17767202f9e350346335cfaf03eb6e941e2e10
Marcelo Chaves <marcelochaves17@gmail.com>
2020-02-12 22:04:18 -0300

    Update Collider class

--- Assets/Source/Modules/CollisionModule/Collider.cs
+++ Assets/Source/Modules/CollisionModule/Collider.cs
@@ -15,11 +15,12 @@ namespace Kinematics.CollisionModule
 
         public PhysicsMaterial material;
 
-        public static List<Collider> colliders = new List<Collider>();
+        public static readonly List<Collider> colliders = new List<Collider>();
 
         protected virtual void Start()
         {
-            position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            UnityEngine.Vector3 positionRef = transform.position;
+            position = new Vector3(positionRef.x, positionRef.y, positionRef.z);
             _center = position + center;
         }
 
@@ -29,46 +30,55 @@ namespace Kinematics.CollisionModule
             _center = position + center;
         }
 
-        public virtual int GetColliderType() => type;
+        private int GetColliderType() => type;
 
         public static Collider CheckCollision(Collider actual, Collider other)
         {
             if (actual == other)
+            {
                 return null;
-
-            if (actual.GetColliderType() == 0 && other.GetColliderType() == 0)
-            {
-                if (Vector3.Magnitude(actual._center - other._center) <= (actual.GetComponent<SphereCollider>().GetRadius() + other.GetComponent<SphereCollider>().GetRadius()))
-                    return other;
             }
-            else if (actual.GetColliderType() == 0 && other.GetColliderType() == 1)
-            {
-                var boxHalf = other.GetComponent<BoxCollider>().GetSize() / 2.0f;
-                var distance = actual.GetComponent<SphereCollider>().GetCenter() - other.GetComponent<BoxCollider>().GetCenter();
-                var cut = Cut(distance, -boxHalf, boxHalf);
-                var next = other.GetComponent<BoxCollider>().GetCenter() + cut;
-                var difference = next - actual.GetComponent<SphereCollider>().GetCenter();
 
-                if (Vector3.Magnitude(difference) <= actual.GetComponent<SphereCollider>().GetRadius())
+            SphereCollider actualSphereComponent = actual.GetComponent<SphereCollider>();
+            BoxCollider actualBoxComponent = actual.GetComponent<BoxCollider>();
+            SphereCollider otherSphereComponent = other.GetComponent<SphereCollider>();
+            BoxCollider otherBoxComponent = actual.GetComponent<BoxCollider>();
+
+            Vector3 boxHalf = otherBoxComponent.GetSize() / 2.0f;
+            Vector3 distance = actualSphereComponent.GetCenter() - otherBoxComponent.GetCenter();
+            Vector3 cut = Cut(distance, -boxHalf, boxHalf);
+            Vector3 next = otherBoxComponent.GetCenter() + cut;
+            Vector3 difference = next - actualSphereComponent.GetCenter();
+
+            int getColliderType = other.GetColliderType();
+
+            if (actual.GetColliderType() == 0 && getColliderType== 0)
+            {
+                if (Vector3.Magnitude(actual._center - other._center) <= actualSphereComponent.GetRadius() + otherSphereComponent.GetRadius())
+                {
                     return other;
+                }
             }
-            else if (actual.GetColliderType() == 1 && other.GetColliderType() == 0)
+            else if (actual.GetColliderType() == 0 && getColliderType == 1)
             {
-                var boxHalf = actual.GetComponent<BoxCollider>().GetSize() / 2.0f;
-                var distance = other.GetComponent<SphereCollider>().GetCenter() - actual.GetComponent<BoxCollider>().GetCenter();
-                var cut = Cut(distance, -boxHalf, boxHalf);
-                var next = actual.GetComponent<BoxCollider>().GetCenter() + cut;
-                var difference = next - other.GetComponent<SphereCollider>().GetCenter();
-
-                if (Vector3.Magnitude(difference) <= other.GetComponent<SphereCollider>().GetRadius())
+                if (Vector3.Magnitude(difference) <= actualSphereComponent.GetRadius())
+                {
                     return other;
+                }
+            }
+            else if (actual.GetColliderType() == 1 && getColliderType == 0)
+            {
+                if (Vector3.Magnitude(difference) <= otherSphereComponent.GetRadius())
+                {
+                    return other;
+                }
             }
             else
             {
-                var actualMin = actual.GetComponent<BoxCollider>().GetMin();
-                var actualMax = actual.GetComponent<BoxCollider>().GetMax();
-                var otherMin = other.GetComponent<BoxCollider>().GetMin();
-                var otherMax = other.GetComponent<BoxCollider>().GetMax();
+                Vector3 actualMin = actualBoxComponent.GetMin();
+                Vector3 actualMax = actualBoxComponent.GetMax();
+                Vector3 otherMin = otherBoxComponent.GetMin();
+                Vector3 otherMax = otherBoxComponent.GetMax();
 
                 if (actualMin.X >= otherMin.X && actualMin.Y >= otherMin.Y && actualMin.Z >= otherMin.Z && actualMin.X <= otherMax.X && actualMin.Y <= otherMax.Y && actualMin.Z <= otherMax.Z)
                 {
@@ -86,27 +96,40 @@ namespace Kinematics.CollisionModule
             return null;
         }
 
-        public virtual Vector3 GetCenter() => _center;
+        public Vector3 GetCenter() => _center;
 
-        public static Vector3 Cut(Vector3 distance, Vector3 min, Vector3 max)
+        private static Vector3 Cut(Vector3 distance, Vector3 min, Vector3 max)
         {
-            float x, y, z;
-            x = distance.X;
-            y = distance.Y;
-            z = distance.Z;
+            float x = distance.X;
+            float y = distance.Y;
+            float z = distance.Z;
 
             if (distance.X > max.X)
+            {
                 x = max.X;
+            }
             else if (distance.X < min.X)
+            {
                 x = min.X;
+            }
+
             if (distance.Y > max.Y)
+            {
                 y = max.Y;
+            }
             else if (distance.Y < min.Y)
+            {
                 y = min.Y;
+            }
+
             if (distance.Z > max.Z)
+            {
                 z = max.Z;
+            }
             else if (distance.Z < min.Z)
+            {
                 z = min.Z;
+            }
 
             return new Vector3(x, y, z);
         }
