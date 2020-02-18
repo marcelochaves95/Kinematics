79e4ea0d7d61d80141b33e9b91cb80932a051bb6
Marcelo Chaves <marcelochaves17@gmail.com>
2020-02-18 19:30:47 -0300

    Update Mathematics

--- Source/Modules/MathModule/Mathematics.cs
+++ Source/Modules/MathModule/Mathematics.cs
@@ -11,7 +11,7 @@ namespace Kinematics.MathModule
         /// The infamous ''3.14159265358979...'' value (RO)
         /// </summary>
         /// <returns>PI</returns>
-        public const float PI = (float)Math.PI;
+        public const float PI = (float) Math.PI;
 
         /// <summary>
         /// A tiny floating point value (Read Only)
@@ -35,42 +35,60 @@ namespace Kinematics.MathModule
         /// </summary>
         /// <param name="value">Angle</param>
         /// <returns></returns>
-        public static float Sin(float value) => (float)Math.Sin(value);
+        public static float Sin(float value)
+        {
+            return (float) Math.Sin(value); 
+        }
 
         /// <summary>
         /// Returns the cosine of angle value in radians
         /// </summary>
         /// <param name="value">Angle</param>
         /// <returns></returns>
-        public static float Cos(float value) => (float)Math.Cos(value);
+        public static float Cos(float value)
+        {
+            return (float) Math.Cos(value);
+        }
 
         /// <summary>
         /// Returns the tangent of angle value in radians.
         /// </summary>
         /// <param name="value">Angle</param>
         /// <returns></returns>
-        public static float Tan(float value) => (float)Math.Tan(value);
+        public static float Tan(float value)
+        {
+            return (float)Math.Tan(value);
+        }
 
         /// <summary>
         /// Returns the arc-sine of value - the angle in radians whose sine is value
         /// </summary>
         /// <param name="value">Angle</param>
         /// <returns></returns>
-        public static float Asin(float value) => (float)Math.Asin(value);
+        public static float Asin(float value)
+        {
+            return (float) Math.Asin(value);
+        }
 
         /// <summary>
         /// Returns the arc-cosine of value - the angle in radians whose cosine is value
         /// </summary>
         /// <param name="value">Angle</param>
         /// <returns></returns>
-        public static float Acos(float value) => (float)Math.Acos(value);
+        public static float Acos(float value)
+        {
+            return (float) Math.Acos(value);
+        }
 
         /// <summary>
         /// Returns the arc-tangent of value - the angle in radians whose tangent is value
         /// </summary>
         /// <param name="value">Angle</param>
         /// <returns></returns>
-        public static float Atan(float value) => (float)Math.Atan(value);
+        public static float Atan(float value)
+        {
+            return (float) Math.Atan(value);
+        }
 
         /// <summary>
         /// Returns the angle in radians whose ::ref::Tan is @@y/x@@.
@@ -78,28 +96,30 @@ namespace Kinematics.MathModule
         /// <param name="y"></param>
         /// <param name="x"></param>
         /// <returns></returns>
-        public static float Atan2(float y, float x) => (float)Math.Atan2(y, x);
+        public static float Atan2(float y, float x)
+        {
+            return (float) Math.Atan2(y, x);
+        }
 
         /// <summary>
         /// Returns square root of value
         /// </summary>
         /// <param name="value">Value</param>
         /// <returns></returns>
-        public static float Sqrt(float value) => (float)Math.Sqrt(value);
+        public static float Sqrt(float value)
+        {
+            return (float) Math.Sqrt(value);
+        }
 
         /// <summary>
         /// Returns the absolute value of value
         /// </summary>
         /// <param name="value">Value</param>
         /// <returns></returns>
-        public static float Abs(float value) => Math.Abs(value);
-
-        /// <summary>
-        /// Returns the absolute value of value
-        /// </summary>
-        /// <param name="value">Value</param>
-        /// <returns></returns>
-        public static int Abs(int value) => Math.Abs(value);
+        public static float Abs(float value)
+        {
+            return (float) Math.Abs(value);
+        }
 
         /// <summary>
         /// 
@@ -107,7 +127,10 @@ namespace Kinematics.MathModule
         /// <param name="value1">First value</param>
         /// <param name="value2">Second value</param>
         /// <returns>The lowest value</returns>
-        public static float Min(float value1, float value2) => value1 < value2 ? value1 : value2;
+        public static float Min(float value1, float value2)
+        {
+            return value1 < value2 ? value1 : value2;
+        }
 
         /// <summary>
         /// 
@@ -115,7 +138,10 @@ namespace Kinematics.MathModule
         /// <param name="value1">First value</param>
         /// <param name="value2">Second value</param>
         /// <returns>The lowest value</returns>
-        public static int Min(int value1, int value2) => value1 < value2 ? value1 : value2;
+        public static int Min(int value1, int value2)
+        {
+            return value1 < value2 ? value1 : value2;
+        }
 
         /// <summary>
         /// 
@@ -123,7 +149,10 @@ namespace Kinematics.MathModule
         /// <param name="value1">First value</param>
         /// <param name="value2">Second value</param>
         /// <returns>The lowest value</returns>
-        public static float Max(float value1, float value2) => value1 > value2 ? value1 : value2;
+        public static float Max(float value1, float value2)
+        {
+            return value1 > value2 ? value1 : value2;
+        }
 
         /// <summary>
         /// 
@@ -131,7 +160,10 @@ namespace Kinematics.MathModule
         /// <param name="value1">First value</param>
         /// <param name="value2">Second value</param>
         /// <returns>The highest value</returns>
-        public static int Max(int value1, int value2) => value1 > value2 ? value1 : value2;
+        public static int Max(int value1, int value2)
+        {
+            return value1 > value2 ? value1 : value2;
+        }
 
         /// <summary>
         /// Returns value raised to power
@@ -139,14 +171,20 @@ namespace Kinematics.MathModule
         /// <param name="value"></param>
         /// <param name="power"></param>
         /// <returns></returns>
-        public static float Pow(float value, float power) => (float)Math.Pow(value, power);
+        public static float Pow(float value, float power)
+        {
+            return (float) Math.Pow(value, power);
+        }
 
         /// <summary>
         /// Returns e raised to the specified power
         /// </summary>
         /// <param name="power"></param>
         /// <returns></returns>
-        public static float Exp(float power) => (float)Math.Exp(power);
+        public static float Exp(float power)
+        {
+            return (float) Math.Exp(power);
+        }
 
         /// <summary>
         /// Returns the logarithm of a specified number in a specified base
@@ -154,27 +192,39 @@ namespace Kinematics.MathModule
         /// <param name="value">Value</param>
         /// <param name="logBase">Base</param>
         /// <returns></returns>
-        public static float Log(float value, float logBase) => (float)Math.Log(value, logBase);
+        public static float Log(float value, float logBase)
+        {
+            return (float) Math.Log(value, logBase);
+        }
 
         /// <summary>
         /// Returns the natural (base e) logarithm of a specified number
         /// </summary>
         /// <param name="value">Value</param>
         /// <returns>Log on base e</returns>
-        public static float Log(float value) => (float)Math.Log(value);
+        public static float Log(float value)
+        {
+            return (float) Math.Log(value);
+        }
 
         /// <summary>
         /// Returns the base 10 logarithm of a specified number
         /// </summary>
         /// <param name="value">Value</param>
         /// <returns>Log on base 10</returns>
-        public static float Log10(float value) => (float)Math.Log10(value);
+        public static float Log10(float value)
+        {
+            return (float) Math.Log10(value);
+        }
 
         /// <summary>
         /// Returns the sign of value
         /// </summary>
         /// <param name="value"></param>
         /// <returns></returns>
-        public static float Sign(float value) => value >= 0f ? 1f : -1f;
+        public static float Sign(float value)
+        {
+            return value >= 0f ? 1f : -1f;
+        }
     }
 }
