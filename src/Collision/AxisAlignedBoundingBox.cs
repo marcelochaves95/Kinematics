ed57628201ac28a1ff59b58f6bd6e1ff4a7f70fa
Marcelo Chaves <marcelochaves17@gmail.com>
2020-07-13 00:59:54 -0300

    Create AxisAlignedBoundingBox (AABB)

---
+++ src/Collision/AxisAlignedBoundingBox.cs
@@ -0,0 +1,98 @@
+using Kinematics.Math;
+
+namespace Kinematics.Collision
+{
+    public struct AxisAlignedBoundingBox
+    {
+        public Vector2 min;
+        public Vector2 max;
+        public bool valid;
+
+        public AxisAlignedBoundingBox(ref Vector2 min, ref Vector2 max)
+        {
+            this.min = min;
+            this.max = max;
+            this.valid = true;
+        }
+
+        public AxisAlignedBoundingBox(Vector2 min, Vector2 max)
+        {
+            this.min = min;
+            this.max = max;
+            this.valid = true;
+        }
+
+        public void Add(float x, float y)
+        {
+            if (valid)
+            {
+                if (x < min.X)
+                {
+                    min.X = x;
+                }
+                else if (x > max.X)
+                {
+                    max.X = x;
+                }
+
+                if (y < min.Y)
+                {
+                    min.Y = y;
+                }
+                else if (y > max.Y)
+                {
+                    max.Y = y;
+                }
+            }
+            else
+            {
+                min.X = max.X = x;
+                min.Y = max.Y = y;
+                valid = true;
+            }
+        }
+
+        public void Clear()
+        {
+            min.X = max.X = min.Y = max.Y = 0;
+            valid = false;
+        }
+
+        public bool Contains(float x, float y)
+        {
+            if (valid)
+            {
+                if ((x < min.X) || (x > max.X))
+                {
+                    return false;
+                }
+
+                if ((y < min.Y) || (y > max.Y))
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Intersects(ref AxisAlignedBoundingBox aabb)
+        {
+            if (this.max.X < aabb.min.X || this.min.X > aabb.max.X)
+            {
+                return false;
+            }
+
+            if (this.max.Y < aabb.min.Y || this.min.Y > aabb.max.Y)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
