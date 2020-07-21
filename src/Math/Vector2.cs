5404a937cd339af7bf1afdce591a955283acab6e
Marcelo Chaves <marcelochaves17@gmail.com>
2020-07-21 18:56:57 -0300

    Refactor Vector2

--- src/Math/Vector2.cs
+++ src/Math/Vector2.cs
@@ -40,19 +40,6 @@ namespace Kinematics.Math
             Y = value.Y;
         }
 
-        public Vector2(Vector4 value)
-        {
-            X = value.X;
-            Y = value.Y;
-        }
-
-        public Vector2(Quaternion value)
-        {
-            (float x, float y, _, _) = value;
-            X = x;
-            Y = y;
-        }
-
         #endregion
 
         #region Operators
@@ -112,16 +99,6 @@ namespace Kinematics.Math
             return new Vector3(value);
         }
 
-        public static explicit operator Vector4(Vector2 value)
-        {
-            return new Vector4(value);
-        }
-
-        public static explicit operator Quaternion(Vector2 value)
-        {
-            return new Quaternion(value);
-        }
-
         #endregion
 
         #region Overrides
@@ -152,12 +129,16 @@ namespace Kinematics.Math
 
         public static Vector2 Barycentric(Vector2 value1, Vector2 value2, Vector2 value3, float amount1, float amount2)
         {
-            return new Vector2(Mathf.Barycentric(value1.X, value2.X, value3.X, amount1, amount2), Mathf.Barycentric(value1.Y, value2.Y, value3.Y, amount1, amount2));
+            float x = Mathf.Barycentric(value1.X, value2.X, value3.X, amount1, amount2);
+            float y = Mathf.Barycentric(value1.Y, value2.Y, value3.Y, amount1, amount2);
+            return new Vector2(x, y);
         }
 
         public static Vector2 CatmullRom(Vector2 value1, Vector2 value2, Vector2 value3, Vector2 value4, float amount)
         {
-            return new Vector2(Mathf.CatmullRom(value1.X, value2.X, value3.X, value4.X, amount), Mathf.CatmullRom(value1.Y, value2.Y, value3.Y, value4.Y, amount));
+            float x = Mathf.CatmullRom(value1.X, value2.X, value3.X, value4.X, amount);
+            float y = Mathf.CatmullRom(value1.Y, value2.Y, value3.Y, value4.Y, amount);
+            return new Vector2(x, y);
         }
 
         public void Ceiling()
@@ -175,21 +156,25 @@ namespace Kinematics.Math
 
         public static Vector2 Clamp(Vector2 value, Vector2 min, Vector2 max)
         {
-            return new Vector2(Mathf.Clamp(value.X, min.X, max.X), Mathf.Clamp(value.Y, min.Y, max.Y));
+            float x = Mathf.Clamp(value.X, min.X, max.X);
+            float y = Mathf.Clamp(value.Y, min.Y, max.Y);
+            return new Vector2(x, y);
         }
 
         public static float Distance(Vector2 value1, Vector2 value2)
         {
-            float v1X = value1.X - value2.X;
-            float v2Y = value1.Y - value2.Y;
-            return Mathf.Sqrt(v1X * v1X + v2Y * v2Y);
+            float x = value1.X - value2.X;
+            float y = value1.Y - value2.Y;
+            Vector2 result = new Vector2(x, y);
+            return Length(result);
         }
 
         public static float DistanceSquared(Vector2 value1, Vector2 value2)
         {
-            float v1X = value1.X - value2.X;
-            float v2Y = value1.Y - value2.Y;
-            return v1X * v1X + v2Y * v2Y;
+            float x = value1.X - value2.X;
+            float y = value1.Y - value2.Y;
+            Vector2 result = new Vector2(x, y);
+            return LengthSquared(result);
         }
 
         public static float Dot(Vector2 value1, Vector2 value2)
@@ -212,49 +197,64 @@ namespace Kinematics.Math
 
         public static Vector2 Hermite(Vector2 value1, Vector2 tangent1, Vector2 value2, Vector2 tangent2, float amount)
         {
-            return new Vector2(Mathf.Hermite(value1.X, tangent1.X, value2.X, tangent2.X, amount), Mathf.Hermite(value1.Y, tangent1.Y, value2.Y, tangent2.Y, amount));
+            float x = Mathf.Hermite(value1.X, tangent1.X, value2.X, tangent2.X, amount);
+            float y = Mathf.Hermite(value1.Y, tangent1.Y, value2.Y, tangent2.Y, amount);
+            return new Vector2(x, y);
         }
 
         public float Length()
         {
-            return Mathf.Sqrt(Mathf.Pow(X, 2) + Mathf.Pow(Y, 2));
+            float lengthSquared = LengthSquared();
+            return Mathf.Sqrt(lengthSquared);
         }
 
-        public static float Length(Vector2 v)
+        public static float Length(Vector2 value)
         {
-            return Mathf.Sqrt(Mathf.Pow(v.X, 2) + Mathf.Pow(v.Y, 2));
+            float lengthSquared = LengthSquared(value);
+            return Mathf.Sqrt(lengthSquared);
         }
 
         public float LengthSquared()
         {
-            return Mathf.Pow(X, 2) + Mathf.Pow(Y, 2);
+            return LengthSquared(this);
+        }
+
+        public static float LengthSquared(Vector2 value)
+        {
+            return Mathf.Pow(value.X, 2) + Mathf.Pow(value.Y, 2);
         }
 
         public static Vector2 Lerp(Vector2 value1, Vector2 value2, float amount)
         {
-            return new Vector2(Mathf.Lerp(value1.X, value2.X, amount), Mathf.Lerp(value1.Y, value2.Y, amount));
+            float x = Mathf.Lerp(value1.X, value2.X, amount);
+            float y = Mathf.Lerp(value1.Y, value2.Y, amount);
+            return new Vector2(x, y);
         }
 
         public static Vector2 LerpPrecise(Vector2 value1, Vector2 value2, float amount)
         {
-            return new Vector2(Mathf.LerpPrecise(value1.X, value2.X, amount), Mathf.LerpPrecise(value1.Y, value2.Y, amount));
+            float x = Mathf.LerpPrecise(value1.X, value2.X, amount);
+            float y = Mathf.LerpPrecise(value1.Y, value2.Y, amount);
+            return new Vector2(x, y);
         }
 
         public static Vector2 Max(Vector2 value1, Vector2 value2)
         {
-            return new Vector2(Mathf.Max(value1.X, value2.X), Mathf.Max(value1.Y, value2.Y));
+            float x = Mathf.Max(value1.X, value2.X);
+            float y = Mathf.Max(value1.Y, value2.Y);
+            return new Vector2(x, y);
         }
 
         public static Vector2 Min(Vector2 value1, Vector2 value2)
         {
-            return new Vector2(Mathf.Min(value1.X, value2.X), Mathf.Min(value1.Y, value2.Y));
+            float x = Mathf.Min(value1.X, value2.X);
+            float y = Mathf.Min(value1.Y, value2.Y);
+            return new Vector2(x, y);
         }
 
         public void Normalize()
         {
-            float length = 1.0f / Length();
-            X *= length;
-            Y *= length;
+            Normalize(this);
         }
 
         public static Vector2 Normalize(Vector2 value)
@@ -267,11 +267,10 @@ namespace Kinematics.Math
 
         public static Vector2 Reflect(Vector2 vector, Vector2 normal)
         {
-            Vector2 result;
-            float val = 2.0f * Dot(vector, normal);
-            result.X = vector.X - normal.X * val;
-            result.Y = vector.Y - normal.Y * val;
-            return result;
+            float dot = Dot(vector, normal);
+            float x = vector.X - 2.0f * dot * normal.X;
+            float y = vector.Y - 2.0f * dot * normal.Y;
+            return new Vector2(x, y);
         }
 
         public void Round()
@@ -289,18 +288,18 @@ namespace Kinematics.Math
 
         public static Vector2 SmoothStep(Vector2 value1, Vector2 value2, float amount)
         {
-            return new Vector2(Mathf.SmoothStep(value1.X, value2.X, amount), Mathf.SmoothStep(value1.Y, value2.Y, amount));
+            float x = Mathf.SmoothStep(value1.X, value2.X, amount);
+            float y = Mathf.SmoothStep(value1.Y, value2.Y, amount);
+            return new Vector2(x, y);
         }
 
         public static Vector2 Rotate(Vector2 value, float radian)
         {
-            Vector2 ret = new Vector2();
             float cos = Mathf.Cos(radian);
             float sin = Mathf.Sin(radian);
-            ret.X = cos * value.X - sin * value.Y;
-            ret.Y = cos * value.Y + sin * value.X;
-
-            return ret;
+            float x = cos * value.X - sin * value.Y;
+            float y = cos * value.Y + sin * value.X;
+            return new Vector2(x, y);
         }
 
         public static Vector2 Perpendicular(Vector2 value)
